2adb72b634182bc596b59959ca78c5e40278048b
Matt Wilkinson <mattwilki17@gmail.com>
2020-09-02 08:39:42 -0400

    find

--- myApp.js
+++ myApp.js
@@ -131,8 +131,11 @@ const createManyPeople = (arrayOfPeople, done) => {
 // It supports an extremely wide range of search options. Check it in the docs.
 // Use the function argument `personName` as search key.
 
-const findPeopleByName = function (personName, done) {
-  done(null /* , data */);
+const findPeopleByName = (personName, done) => {
+  Person.find({ name: 'Matt' }, (err, data) => {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 /** 6) Use `Model.findOne()` */
